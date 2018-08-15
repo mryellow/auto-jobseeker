908b3400aa65c42f5e4a7370846bf39b980f341a
Mr-Yellow <mr-yellow@mr-yellow.com>
2018-08-15 14:49:42 +1000

    Save to PouchDB

--- server.js
+++ server.js
@@ -4,24 +4,29 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
-//const md5 = require('md5');
-//const path = require('path');
-const IPFS = require('ipfs');
-const OrbitDB = require('orbit-db');
+const PouchDB = require('pouchdb-node');
+
+PouchDB.plugin(require('pouchdb-adapter-leveldb')).plugin(
+  require('pouchdb-adapter-http')
+);
 
 const Gmail = require('./gmail.js');
 const gmail = new Gmail(JSON.parse(process.env.GOOGLE_CREDS));
 
-const ipfsOptions = {
-  EXPERIMENTAL: {
-    pubsub: true
-  }
-};
-
-const ipfs = new IPFS(ipfsOptions);
-
 const port = process.env.PORT || 3000;
 //const debug = process.env.NODE_ENV !== 'production';
+
+let pdb;
+if (process.env.DBHOST) {
+  pdb = new PouchDB(
+    'https://' + process.env.DBHOST + '/' + process.env.DBNAME,
+    {
+      auth: { username: process.env.DBUSER, password: process.env.DBPASS }
+    }
+  );
+} else {
+  pdb = new PouchDB(process.env.DBNAME);
+}
 
 app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -45,11 +50,12 @@ app.use(httpsRedirect);
 
 // Configure view engine
 app.engine('html', function(filePath, options, callback) {
+  const layout = fs.readFileSync('views/layouts/default.html');
   const delimiter = '%%';
   fs.readFile(filePath, function(err, content) {
     if (err) return callback(err);
 
-    let rendered = content.toString();
+    let rendered = layout.toString().replace('%%content%%', content.toString());
     for (let key in options) {
       if (options.hasOwnProperty(key) && typeof options[key] === 'string') {
         rendered = rendered.replace(delimiter + key + delimiter, options[key]);
@@ -61,47 +67,43 @@ app.engine('html', function(filePath, options, callback) {
 app.set('views', './views');
 app.set('view engine', 'html');
 
-ipfs.on('error', e => console.error(e));
-ipfs.on('ready', async () => {
-  const orbitdb = new OrbitDB(ipfs);
-  const db = await orbitdb.kvstore('settings');
-  await db.load();
+app.get('/', function(req, res) {
+  const googleAuthUrl = gmail.getAuthUrl();
+  const googleAuthLink = googleAuthUrl
+    ? '<a href="' + googleAuthUrl + '">Authorise</a>'
+    : '';
 
-  app.get('/', function(req, res) {
-    const googleAuthUrl = gmail.getAuthUrl();
-    const googleAuthLink = googleAuthUrl
-      ? '<a href="' + googleAuthUrl + '">Authorise</a>'
-      : '';
+  res.render('index', { GoogleAuthLink: googleAuthLink });
+});
 
-    res.render('index', { GoogleAuthLink: googleAuthLink });
-  });
+app.get('/auth/google/callback', function(req, res) {
+  res.render('callback', { code: req.query.code });
+});
 
-  app.get('/auth/google/callback', function(req, res) {
-    res.render('callback', { code: req.query.code });
-  });
+app.post('/auth/google/callback', async function(req, res) {
+  req.body.token = await gmail.getToken(req.body.code);
+  delete req.body.code;
 
-  app.post('/auth/google/callback', async function(req, res) {
-    req.body.token = await gmail.getToken(req.body.code);
-    delete req.body.code;
+  const existing = await pdb.get(req.body._id);
+  if (existing._rev) req.body._rev = existing._rev;
+  pdb
+    .put(req.body)
+    .then(function(response) {
+      console.log('Jobseeker Saved', response.ok);
+      res.redirect('/');
+    })
+    .catch(function(err) {
+      console.log(err);
+      res.status(err.status || 500);
+      res.redirect('/');
+    });
+});
 
-    let length = db.get('length') || 0;
-    await db.put('length', ++length);
-    await db.put(length + '_id', req.body);
-    console.log(
-      '%s: Saved Jobseeker #%d ID: %s',
-      new Date().toISOString(),
-      length,
-      db.get(length + '_id').id
-    );
-    res.redirect('/');
-  });
+// Log errors
+app.use(function(err, req, res, next) {
+  console.error(err);
+});
 
-  // Log errors
-  app.use(function(err, req, res, next) {
-    console.error(err);
-  });
-
-  app.listen(port, function() {
-    console.log('Listening on port ' + port + '!');
-  });
+app.listen(port, function() {
+  console.log('Listening on port ' + port + '!');
 });
